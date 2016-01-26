11ff60d4cd63545cd462793cd15348908fe69108
Carlos Gomez Briones <car_25_ixco@hotmail.com>
2016-01-25 21:21:57 -0600

    Adding route skeleton

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import {createProxyServer} from "http-proxy";
 
 const proxy = createProxyServer({});
 
+var self = null;
+
 class Rack{
   constructor(options){
+    self = this;
+
     let server = createServer((req,res) => {
 
     });
 
     console.log(`Server is listening at port : ${options.port || 8000}`);
     server.listen(options.port || 8000);
+  }
+  route(route,params){
+    console.log(self.route);
   }
 }
 
